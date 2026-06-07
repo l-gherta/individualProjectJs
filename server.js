352c27a3a1da08f2f692da80754389da9825f47e
lgherta <ghertaliliandeveloper2006@gmail.com>
2026-06-08 00:20:11 +0300

    fix

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require("http");
 const fs = require("fs");
 const path = require("path");
 
-const PORT = Number(process.env.PORT) || 8080;
+const PORT = Number(process.env.PORT) || 8082;
 const ROOT_DIR = __dirname;
 
 const MIME_TYPES = {
